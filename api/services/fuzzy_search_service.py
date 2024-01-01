6bc85a0af1fbb99ec387bcea91dd9edba55ce7d4
Naren Mudivarthy <narenuday595@gmail.com>
2024-01-01 02:01:37 -0800

    All is well

--- api/services/fuzzy_search_service.py
+++ api/services/fuzzy_search_service.py
@@ -11,23 +11,23 @@ class FuzzySearchService:
     def search_sloka_fuzzy(self, query):
         query = query.lower()  # Convert query to lowercase
 
+        results = []
         for kanda_number, kanda in self.ramayanam_data.kandas.items():
             self.logger.debug("Kadna %s", kanda)
             for sarga_number, sarga in kanda.sargas.items():
-                sarga = kanda.sargas.get(sarga_number)
                 if not sarga:
                     self.logger.error(f"Sarga '{sarga_number}' not found for Kanda '{kanda_number}'")
                     continue
 
-                results = []
                 for sloka_number, sloka in sarga.slokas.items():
                     if sloka is None or sloka.translation is None:
                         self.logger.debug("Kanda %s, Sarga %s, Sloka %s", kanda, sarga, sloka)
                         continue
                     text = sloka.translation.lower()  # Convert sloka text to lowercase
-                    ratio = fuzz.ratio(text, query)
+                    # ratio = fuzz.ratio(text, query)
+                    ratio = fuzz.partial_ratio(text, query)
                     self.logger.debug(f"Checking sloka {kanda_number}.{sarga_number}.{sloka_number} - Ratio: {ratio}")
-                    if ratio > 5:  # Adjust the threshold as needed
+                    if ratio > 50:  # Adjust the threshold as needed
                         results.append({"sloka_number": sloka.id, "sloka":sloka.translation, "ratio": ratio})
 
         results.sort(key=lambda x: x["ratio"], reverse=True)
