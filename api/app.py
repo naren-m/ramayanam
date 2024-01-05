0d5f8eb00194b0806f15acc8ffeb6c1cdee142d1
Naren Mudivarthy <narenuday595@gmail.com>
2024-01-04 16:05:53 -0800

    Added tests

--- api/app.py
+++ api/app.py
@@ -1,14 +1,18 @@
 from flask import Flask, send_file
 from api.controllers.sloka_controller import sloka_blueprint
 import logging
+import tracemalloc
 
+tracemalloc.start()
 # Configure logging
 logging.basicConfig(level=logging.DEBUG)
 
 # app = Flask(__name__)
-app = Flask(__name__,
-            static_url_path='/static',
-            static_folder='../slokas-frontend')
+app = Flask(__name__)
+
+
+# Add this for testing
+test_client = app.test_client
 
 # Register the sloka_blueprint
 app.register_blueprint(sloka_blueprint, url_prefix='/api/ramayanam')
---
+++ tests/tests.py
@@ -0,0 +1,25 @@
+# tests.py
+
+import unittest
+# from ..api import app
+
+import sys
+sys.path.append("/home/narenuday/Projects/ramayanam")
+
+from api import app
+
+class TestFlaskApp(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_home_page(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_search_sloka_fuzzy(self):
+        response = self.app.get('/api/ramayanam/slokas/fuzzy-search?query=hanuman')
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
+
